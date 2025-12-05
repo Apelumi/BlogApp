5cbe539f6cf221f66223ad4326f1ab55d2e0d9cc
Adeyefa Oluwapelumi <beefarsh@localhost.localdomain>
2025-12-05 10:40:17 +0100

    the postshare form input, rendering and validation

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404, request
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .form import EmailPostForm
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
 
 def post_list(request):
     post_lists = Post.published.all()
@@ -45,4 +54,25 @@ def post_detail(request, year, month, day, post):
         request,
         'blog/post/detail.html',
         {"post": post}
+    )
+
+# form rendering and form validation
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id = post_id,
+        status = Post.Status.PUBLISHED
+    )
+    if request.method == "POST":
+        form = EmailPostForm(request.post)
+        if form.is_valid():
+            cleanData = form.cleaned_data
+        else:
+            form = EmailPostForm()
+
+    return render(
+        request,
+        "blog/post/share.html",
+        {"post": post,
+         "form": form}
     )
